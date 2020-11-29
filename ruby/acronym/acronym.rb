4c4ff52f215c048f90ba9875e80286aff09f79cb
IkumaTadokoro <tadokorodev@gmail.com>
2020-11-29 16:45:53 +0900

    Use String#scan instead of split & chr

--- ruby/acronym/acronym.rb
+++ ruby/acronym/acronym.rb
@@ -8,6 +8,7 @@ To get started with TDD, see the `README.md` file in your
 
 class Acronym
   def self.abbreviate(long_name)
-    long_name.split(/\W/).map(&:chr).join.upcase
+    # long_name.split(/\W/).map(&:chr).join.upcase
+    long_name.scan(/\b\w/).join.upcase
   end
 end
