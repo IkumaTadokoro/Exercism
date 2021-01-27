d1ed4e551166b9e173ceba70aa45cb448e2f0ba5
IkumaTadokoro <tadokorodev@gmail.com>
2021-01-27 23:08:47 +0900

    TwleveDays仮実装

--- ruby/twelve-days/twelve_days.rb
+++ ruby/twelve-days/twelve_days.rb
@@ -7,34 +7,48 @@ To get started with TDD, see the `README.md` file in your
 =end
 
 class TwelveDays
-
-  @@txt = <<~'EOS'
-    On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.
-
-    On the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree.
-
-    On the third day of Christmas my true love gave to me: three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.
-
-    On the fourth day of Christmas my true love gave to me: four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.
-
-    On the fifth day of Christmas my true love gave to me: five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.
-
-    On the sixth day of Christmas my true love gave to me: six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.
-
-    On the seventh day of Christmas my true love gave to me: seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.
-
-    On the eighth day of Christmas my true love gave to me: eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.
-
-    On the ninth day of Christmas my true love gave to me: nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.
-
-    On the tenth day of Christmas my true love gave to me: ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.
-
-    On the eleventh day of Christmas my true love gave to me: eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.
-
-    On the twelfth day of Christmas my true love gave to me: twelve Drummers Drumming, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.
-  EOS
+  GIFTS = [
+    ['first', 'a Partridge in a Pear Tree'],
+    ['second', 'two Turtle Doves'],
+    ['third', 'three French Hens'],
+    ['fourth', 'four Calling Birds'],
+    ['fifth', 'five Gold Rings'],
+    ['sixth', 'six Geese-a-Laying'],
+    ['seventh', 'seven Swans-a-Swimming'],
+    ['eighth', 'eight Maids-a-Milking'],
+    ['ninth', 'nine Ladies Dancing'],
+    ['tenth', 'ten Lords-a-Leaping'],
+    ['eleventh', 'eleven Pipers Piping'],
+    ['twelfth', 'twelve Drummers Drumming']
+  ]
 
   def self.song
-    @@txt
+    new.song
   end
+
+  def song
+    GIFTS.size.times.map{ |n| verse(n) }.join("\n")
+  end
+
+  private
+
+  def verse(n)
+    "On the #{day(n)} day of Christmas my true love gave to me: #{build_lyrics(gifts(n))}.\n"
+  end
+
+  def day(n)
+    GIFTS[n].first
+  end
+
+  def gifts(n)
+    GIFTS.take(n + 1).map(&:last)
+  end
+
+  def build_lyrics(gifts)
+    return gifts.first if gifts.one?
+
+    first_gift, *after_second_gifts = gifts
+    "#{after_second_gifts.reverse.join(', ')}, and #{first_gift}"
+  end
+
 end
