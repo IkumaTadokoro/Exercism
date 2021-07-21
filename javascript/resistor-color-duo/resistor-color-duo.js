3b9b97d7313620b15d6ba0b23a8af42c96da1dd1
IkumaTadokoro <tadokorodev@gmail.com>
2021-07-22 08:36:46 +0900

    decodedValueを実装

--- javascript/resistor-color-duo/resistor-color-duo.js
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -3,6 +3,20 @@
 // convenience to get you started writing code faster.
 //
 
-export const decodedValue = () => {
-  throw new Error('Remove this statement and implement this function');
+export const decodedValue = (colorName) => {
+  const [color1, color2] =　colorName;  
+  return parseInt(`${COLORS[color1]}${COLORS[color2]}`)
 };
+
+export const COLORS = {
+  'black': 0,
+  'brown': 1,
+  'red': 2,
+  'orange': 3,
+  'yellow': 4,
+  'green': 5,
+  'blue': 6,
+  'violet': 7,
+  'grey': 8,
+  'white': 9,
+}
