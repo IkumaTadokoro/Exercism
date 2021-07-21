f055a122f7c02fe39fd05377b84d585d89761de2
IkumaTadokoro <tadokorodev@gmail.com>
2021-07-22 05:53:35 +0900

    配列を用いて実装

--- javascript/resistor-color/resistor-color.js
+++ javascript/resistor-color/resistor-color.js
@@ -4,18 +4,18 @@
 //
 
 export const colorCode = (colorName) => {
-  return COLORS[colorName];
+  return COLORS.indexOf(colorName);
 };
 
-export const COLORS = {
-  'Black' : 0,
-  'Brown' : 1,
-  'Red' : 2,
-  'Orange' : 3,
-  'Yellow' : 4,
-  'Green' : 5,
-  'Blue' : 6,
-  'Violet' : 7,
-  'Grey' : 8,
-  'White' : 9,
-};
+export const COLORS = [
+  'black',
+  'brown',
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  'violet',
+  'grey',
+  'white',
+];
