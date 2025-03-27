e2f20260bd2eab67198792ba698c4448ca5bf67b
ikuma-t <tadokorodev@gmail.com>
2025-03-27 21:35:41 +0900

    haskell: acronym2

--- haskell/acronym/src/Acronym.hs
+++ haskell/acronym/src/Acronym.hs
@@ -3,20 +3,17 @@ module Acronym (abbreviate) where
 import Data.Char (isAlpha, isUpper, toUpper)
 
 abbreviate :: String -> String
-abbreviate = concatMap initials . words . map replaceNonWord
+abbreviate = concatMap initials . words . sanitize
 
 initials :: String -> String
 initials [] = []
-initials (x:xs) = toUpper x : rest
-  where rest = if not $ isAcronym xs
-               then filter isUpper xs
-               else []
+initials (x:xs) = toUpper x : if all isUpper xs then [] else filter isUpper xs
 
-isAcronym :: String -> Bool
-isAcronym = all isUpper
-
-replaceNonWord :: Char -> Char
-replaceNonWord '\'' = '\''
-replaceNonWord x
-  | isAlpha x = x
-  | otherwise = ' '
+sanitize :: String -> String
+sanitize = map replaceNonAlpha
+  where
+    replaceNonAlpha :: Char -> Char
+    replaceNonAlpha '\'' = '\''
+    replaceNonAlpha x
+      | isAlpha x = x
+      | otherwise = ' '
